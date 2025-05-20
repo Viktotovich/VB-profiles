f7ea4b4eaeb85d6eae3392901924a127808464c5
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-05-20 16:45:35 +0400

    Fixed all cloudflare adapter related build bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
   integrations: [react()],
 
+  //https://github.com/withastro/astro/issues/12824 << bane of my existence
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ["react-dom"], // possibly a fix for Cloudflare
+    },
+    resolve: {
+      alias: {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
+
+  output: "server",
 });
