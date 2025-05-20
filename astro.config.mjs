6277c2eab3a16128cf004b375811c6d518b8382d
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-05-20 18:14:22 +0400

    Removed React from the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,15 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
-import react from "@astrojs/react";
-
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  integrations: [react()],
 
   //https://github.com/withastro/astro/issues/12824 << bane of my existence
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "react-dom/server": "react-dom/server.edge",
-      },
-    },
   },
 
   output: "server",
